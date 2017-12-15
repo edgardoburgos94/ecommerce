2f7e6576ae28e04e24e3425ae3d9a61e4efd59fd
Edgardo David <edgardo.burgos94@gmail.com>
2017-12-15 18:28:48 -0500

    Run migrations Category, Product and Size

--- db/migrate/20171215223830_create_categories.rb
+++ db/migrate/20171215223830_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
       t.string :title
-      t.string :img-url
+      t.string :img_url
 
       t.timestamps
     end
