1d49aa637d5339a4d86452ef186e2f5e96f4501d
Edgardo David <edgardo.burgos94@gmail.com>
2017-12-15 18:33:55 -0500

    Add and run annotate gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  img_url    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
 end
--- test/models/size_test.rb
+++ test/models/size_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: sizes
+#
+#  id         :integer          not null, primary key
+#  product_id :integer
+#  xs         :integer
+#  s          :integer
+#  m          :integer
+#  l          :integer
+#  xl         :integer
+#  xxl        :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_sizes_on_product_id  (product_id)
+#
+
 require 'test_helper'
 
 class SizeTest < ActiveSupport::TestCase
