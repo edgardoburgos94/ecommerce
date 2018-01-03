544fcd25d68dbb03f8ab273f8c261bcc37d7ef91
Edgardo David <edgardo.burgos94@gmail.com>
2018-01-03 14:10:53 -0500

    Add ajax when a new item is added to the cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,7 +4,7 @@ class OrderItemsController < ApplicationController
 		@order_item = @order.order_items.new(order_item_params)
 		@order.save
 		session[:order_id] = @order.id
-		redirect_to request.referrer
+		
 	end
 
 	def update
