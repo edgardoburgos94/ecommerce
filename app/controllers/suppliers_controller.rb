9b8c7257c50df117d28df76eed9c8fac18f368a4
Edgardo David <edgardo.burgos94@gmail.com>
2018-01-04 01:17:22 -0500

    Add user details view

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -2,7 +2,7 @@ class SuppliersController < ApplicationController
   def show
     @supplier = Supplier.find(params[:id])
   end
-  
+
   def edit
   end
 
@@ -11,7 +11,7 @@ class SuppliersController < ApplicationController
     @supplier.destroy
 
     if @supplier.destroy
-        redirect_to suppli_list_path, notice: "Supplier deleted."
+        redirect_to suppli_list_path, notice: "Proveedor eliminado"
     end
   end
 end
