63b0ed51e6149a19148273913f787a7fb21f92d3
Edgardo David <edgardo.burgos94@gmail.com>
2018-01-16 12:11:38 -0500

    Remove jquery from product show

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,11 +1,1 @@
-function SizeQuantity() {
 
-  $('.size-selector').on('click', function() {
-  var form_field_value = $(".size-selector").val()
-  console.log(form_field_value);
-  if ($(".amount").val()>form_field_value) {
-    console.log("No está permitido");
-  };
-  });
-
-}
