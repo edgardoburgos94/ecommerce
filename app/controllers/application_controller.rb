b7b1981a646e4f246eef4addd21b37c696240cc9
Edgardo David <edgardo.burgos94@gmail.com>
2018-01-26 14:21:36 -0500

    Add funcionability to links in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout 'application'
+  before_action :set_layout_variables
+  def set_layout_variables
+    @categories = Category.all
+  end
 
   include ApplicationHelper
 
