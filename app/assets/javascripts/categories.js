7c91cdeab653cf60d7a3d66aaf6ff6c9f244f9eb
Edgardo David <edgardo.burgos94@gmail.com>
2018-01-30 12:45:08 -0500

    Add cover and profile image in supplier profile

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -3,4 +3,8 @@ function ResizeProductImg() {
     var cw = $('.adjust-product-img').width();
     $(".adjust-product-img").css({'height':cw*1.1182+'px'});
   });
+  $(document).ready(function(){
+    var cw = $('.adjust-product-img').width();
+    $(".adjust-product-img").css({'height':cw*1.1182+'px'});
+  });
 }
--- app/assets/javascripts/suppliers.js
+++ app/assets/javascripts/suppliers.js
@@ -1,1 +1,11 @@
+function ResizeSupplierCover() {
+  $( window ).resize(function() {
+    var width = $('.supplier-cover').width();
+    $(".supplier-cover").css({'height':width*0.33+'px'});
+  });
 
+  $(document).ready(function(){
+    var width = $('.supplier-cover').width();
+    $(".supplier-cover").css({'height':width*0.33+'px'});
+  });
+}
