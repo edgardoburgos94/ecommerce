5d93caa82aef1ad86214faee62a25e81010592c1
Edgardo David <edgardo.burgos94@gmail.com>
2018-02-14 18:40:22 -0500

    Show category icons in a rectangle

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -8,3 +8,14 @@ function ResizeProductImg() {
     $(".adjust-product-img").css({'height':cw*1.1182+'px'});
   });
 }
+
+function ResizeCategoryIcon() {
+  $( window ).resize(function() {
+    var cw = $('.adjust-category-icon').width();
+    $(".adjust-category-icon").css({'height':cw*1.1+'px'});
+  });
+  $(document).ready(function(){
+    var cw = $('.adjust-category-icon').width();
+    $(".adjust-category-icon").css({'height':cw*1.1+'px'});
+  });
+}
