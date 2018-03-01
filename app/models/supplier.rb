92d1f68db99e0f14a55c674d1f3095a31baf29df
Edgardo David <edgardo.burgos94@gmail.com>
2018-03-01 18:30:26 -0500

    Add promotion table and shipping table to supplier#new and supplier#edit

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -87,7 +87,8 @@ end
 class Supplier::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
-    permit(:sign_up, keys: [:full_name, :email, :password, :password_confirmation, :country, :city, :dir, :cel])
-    permit(:account_update, keys: [:full_name, :email, :password, :password_confirmation, :country, :city, :dir, :cel, :profile_img, :cover_img])
+    sign_in_permit = [:full_name, :email, :password, :password_confirmation, :country, :state, :city, :dir, :cel, :q1, :q2, :q3, :q4, :p_q1, :p_q2, :p_q3, :p_q4, :p1, :p2, :p3, :p4, :p_p1, :p_p2, :p_p3, :p_p4, :sq1, :sq2, :sq3, :sq4, :sp_q1, :sp_q2, :sp_q3, :sp_q4, :sp1, :sp2, :sp3, :sp4, :sp_p1, :sp_p2, :sp_p3, :sp_p4, :profile_img, :cover_img]
+    permit(:sign_up, keys: sign_in_permit)
+    permit(:account_update, keys: sign_in_permit)
   end
 end
