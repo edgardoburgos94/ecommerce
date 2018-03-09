d8c6a3ff17cead854f04478eee5f6c356d48549c
Edgardo David <edgardo.burgos94@gmail.com>
2018-03-09 09:57:39 -0500

    Fix edition in shopping cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,7 +6,7 @@ class OrderItemsController < ApplicationController
     if @order.list_groups.where(list_id: product_list_id).length == 1
       @order_item = @order.list_groups.where(list_id: product_list_id).first.order_items.new(order_item_params)
     else
-      @list = @order.list_groups.create(list_id: product_list_id)
+      @list = @order.list_groups.new(list_id: product_list_id)
       @order_item = @list.order_items.new(order_item_params)
     end
 
@@ -30,10 +30,9 @@ class OrderItemsController < ApplicationController
 
 	def update
 		@order = current_order
-		@order_item = @order.order_items.find(params[:id])
+		@order_item = OrderItem.find(params[:id])
 		@order_item.update_attributes(order_item_params)
-		@order_items = @order.order_items
-
+    @list_groups = current_order.list_groups
 	end
 
 	def show
