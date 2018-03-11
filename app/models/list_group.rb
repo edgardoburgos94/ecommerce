c0177321c97c9c320155f00c7d144865bb806418
Edgardo David <edgardo.burgos94@gmail.com>
2018-03-11 16:31:06 -0500

    Shopping cart working with discounts

--- app/models/list_group.rb
+++ app/models/list_group.rb
@@ -42,7 +42,61 @@ class ListGroup < ApplicationRecord
 
   def list_discount
     if list.promotion == true
-      self[:quantity]
+      if list.price == true
+        discount_vals = [list.p1, list.p2, list.p3, list.p4]
+        discount_percents = [list.p_p1,list.p_p2,list.p_p3,list.p_p4]
+        ranges = number_of_ranges(discount_vals)
+        puts("#{ranges}<----------------------------------------------")
+        puts("Precios de envpio: #{discount_percents}<----------------------------------------------")
+        puts("Valores de envío: #{discount_vals}<----------------------------------------------")
+
+        ranges
+        shipping_val=nil
+        (ranges).times do |i|
+          puts(i)
+          puts("Comparación cantidad con rangos: #{self[:subtotal]} >= #{discount_vals[i]} <---------------------------------------------")
+          if discount_vals[i] <= list_sub_total
+            puts("Entró #{discount_percents[i]}")
+            shipping_val = discount_percents[i]
+            puts("shipping_val #{shipping_val}")
+          end
+        end
+        puts("#{shipping_val.class}<----------------------------------------------")
+        if shipping_val.class == Float
+          puts("DESCUENTO APLICADO : #{shipping_val}<----------------------------------------------")
+          shipping_val
+        else
+          puts("DESCUENTO APLICADO NINGUNO: 0%<----------------------------------------------")
+          0
+        end
+      else
+        discount_vals = [list.q1, list.q2, list.q3, list.q4]
+        discount_percents = [list.p_q1,list.p_q2,list.p_q3,list.p_q4]
+        ranges = number_of_ranges(discount_vals)
+        puts("#{ranges}<----------------------------------------------")
+        puts("Precios de envpio: #{discount_percents}<----------------------------------------------")
+        puts("Valores de envío: #{discount_vals}<----------------------------------------------")
+
+        ranges
+        shipping_val=nil
+        (ranges).times do |i|
+          puts(i)
+          puts("Comparación cantidad con rangos: #{self[:quantity]} >= #{discount_vals[i]} <---------------------------------------------")
+          if discount_vals[i] <= list_quantity
+            puts("Entró #{discount_percents[i]}")
+            shipping_val = discount_percents[i]
+            puts("shipping_val #{shipping_val}")
+          end
+        end
+        puts("#{shipping_val.class}<----------------------------------------------")
+        if shipping_val.class == Float
+          puts("DESCUENTO APLICADO : #{shipping_val}<----------------------------------------------")
+          shipping_val
+        else
+          puts("DESCUENTO APLICADO NINGUNO: 0%<----------------------------------------------")
+          0
+        end
+      end
     else
       0
     end
@@ -91,8 +145,8 @@ class ListGroup < ApplicationRecord
         shipping_val=nil
         (ranges).times do |i|
           puts(i)
-          puts("Comparación cantidad con rangos: #{self[:quantity]} >= #{shipping_vals[i]} <---------------------------------------------")
-          if shipping_vals[i] <= list_quantity
+          puts("Comparación cantidad con rangos: #{self[:subtotal]} >= #{shipping_vals[i]} <---------------------------------------------")
+          if shipping_vals[i] <= list_sub_total
             puts("Entró #{shipping_prices[i]}")
             shipping_val = shipping_prices[i]
             puts("shipping_val #{shipping_val}")
@@ -115,7 +169,7 @@ class ListGroup < ApplicationRecord
     puts("Valores de envío #{vals}")
     vals.each do |val|
       puts("Valor de envío #{val.class}")
-      if val.class == Fixnum
+      if val.class == Fixnum || val.class == Float
         puts("Valor de envío #{val}")
         puts(ranges)
         ranges=ranges+1
